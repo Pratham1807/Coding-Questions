6ec1c8cb18e88c755c187b4c0f4aec6dd9cc5f8d
Dvd1234 <deepak.dwivedi964@gmail.com>
2021-10-31 22:11:35 +0530

    Added Day 18- Deepak Returns ka bhi Returns :P

---
+++ October/Day18.java
@@ -0,0 +1,14 @@
+QUESTION : 
+Pratham was very very happy today and had an array of size n consisting of only 0's and 1's.
+The array is sorted. Find the count of all the 0's.
+
+Input:
+n = 12
+arr[] = {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+
+Output: 3
+
+Explanation: There are 3 0's in the given array.
+
+SOLUTION:
+Huraayyyyy He became happierrrrrr!!
